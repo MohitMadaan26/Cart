877e3b8b24d5c1a89b93d65ca5a775d3885bd382
Mohit Madaan <mmadaan171@gmail.com>
2023-06-27 07:09:32 +0530

    Complete All the Functionlaity

--- src/App.js
+++ src/App.js
@@ -1,16 +1,147 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CartItem from './CartItem'
 import Cart from './Cart';
+import { render } from '@testing-library/react';
+import Navbar from './NavBar';
 
 
 
 
-function App() {
-  return (
-    <div className="App">
-     <Cart />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor (){
+    super();
+    this.state = {
+        products : [
+            {
+             price : 99,
+             title : 'Watch',
+             qty : 1,
+             img : 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60',
+             id : 1
+            },
+             {
+                price : 999,
+                title : 'Mobile',
+                qty : 10,
+             img : 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60',
+                id : 2,         
+             },
+             {
+                price : 99,
+                title : 'Laptop',
+                qty : 4,
+                img : 'https://images.unsplash.com/photo-1575024357670-2b5164f470c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGxhcHRvcHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+                id : 3
+             },
+        ]
+    }
+}
+
+handleDeleteItem = (id) =>{
+    const {products} = this.state;
+
+    const items = products.filter((item) => item.id !== id);
+
+    this.setState({
+        products : items
+    })
+
+}
+
+handleDecreaseQuantity = (product) =>{
+
+    console.log("decrease quanity of this", product);
+    const {products} = this.state;
+    const index = products.indexOf(product);
+    const quanity = products[index].qty;
+
+    if(quanity === 0){
+        return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+        products : products
+    })
+
+}
+
+handleIncreaseQuantity = (product) =>{
+    console.log("increase quanity of this", product);
+    const {products} = this.state;
+    const index = products.indexOf(product);
+    products[index].qty += 1;
+
+    this.setState({
+        products : products
+    })
+}
+
+getCartCount = (product) =>{
+
+  const {products} = this.state;
+
+  let count = 0;
+
+  products.forEach((product) =>{
+
+    count += product.qty;
+
+  })
+
+
+  return count;
+}
+
+getCartTotal = (product) =>{
+
+  const {products} = this.state;
+
+  var totalPrice = 0;
+
+  products.forEach((product) =>{
+      totalPrice += product.qty * product.price;
+  })
+
+  return totalPrice;
+
+}
+    render(){
+
+      const {products} = this.state;
+
+      return (
+        <div className="App">
+        <Navbar 
+         count = {this.getCartCount()}
+        />  
+        <Cart
+        products = {products}
+        onIncreaseQuantity={this.handleIncreaseQuantity}
+        onDecreaseQuantity={this.handleDecreaseQuantity}
+        onDeleteItem={this.handleDeleteItem}
+        />
+
+<div style={style.TotalCart}> TOTAL : {this.getCartTotal()} </div>
+       
+        </div>
+
+        
+      );
+    }
+  }
+
+  const style = {
+
+    TotalCart : {
+      fontSize: 20,
+      padding: 10,
+      backgroundColor: '#dcd939',
+      height: 50,
+      width: 100,
+  }
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -5,87 +5,54 @@ const MinusImage = require('./image/minus-sign.png');
 const DeleteImage = require('./image/delete.png');
 
 
-class CartItem extends React.Component{
+const CartItem = (props) =>{
 
-    increaseQuantity = () => {
-        
-        // setState method one !
+    const {price, qty, title} = props.product;
 
-        // this.setState({
-        //     qty : this.state.qty + 1,
-        // });
+    const{
+          onIncreaseQuantity,
+          onDecreaseQuantity,
+          product,
+          onDeleteItem
+        } = props;
+    return(
+        <div className="cart-item">
 
-
-        // setState method second -- Use only when previous state is available
-        this.setState((prevState) => {
-            return {
-                qty : prevState.qty + 1
-            }
-        });
-    }
-
-    decreaseQuantity = () => {
-
-        var quantity = this.state.qty;
-
-        if(quantity === 0){
-            return;
-        }
-
-        this.setState((prevState) => {
-            return {
-                qty : prevState.qty - 1
-            }
-        });
-        
-
-    }
-
-    render(){
-
-        const {price, qty, title} = this.props.product;
-
-        const{onDecreaseQuantity, product, onDeleteItem} = this.props;
-        return(
-            <div className="cart-item">
-
-                <div className="left-block">
-                    <img style={style.image}></img>
-                </div>
-
-                <div className="right-block">
-                    <div style={{fontSize: 25 }}>{title}</div>
-                    <div style={{color: '#777'}}>Rs {price}</div>
-                    <div style={{color: '#777'}}>Qty: {qty}</div>
-                    <div className="cart-item-actions">
-                       <img alt="increase" 
-                       className="action-icons" 
-                       src= {PlusImage} 
-                       onClick= {() => this.props.onIncreaseQuantity(this.props.product)}
-                       />
-                       <img alt="decrease" 
-                       className="action-icons" 
-                       src= {MinusImage}
-                       onClick={() => onDecreaseQuantity(product)}
-                       />
-                       <img alt="delete" 
-                       className="action-icons" 
-                       src= {DeleteImage} 
-                       onClick={() => onDeleteItem(product.id)}
-                       />
-                    </div>
-                </div>
-
-
+            <div className="left-block">
+                <img src = {product.img} style={style.image}   />
             </div>
 
-        );
+            <div className="right-block">
+                <div style={{fontSize: 25 }}>{title}</div>
+                <div style={{color: '#777'}}>Rs {price}</div> 
+                <div style={{color: '#777'}}>Qty: {qty}</div>
+                <div className="cart-item-actions">
+                    <img alt="increase" 
+                    className="action-icons" 
+                    src= {PlusImage} 
+                    onClick= {() => onIncreaseQuantity(product)}
+                    />
+                    <img alt="decrease" 
+                    className="action-icons" 
+                    src= {MinusImage}
+                    onClick={() => onDecreaseQuantity(product)}
+                    />
+                    <img alt="delete" 
+                    className="action-icons" 
+                    src= {DeleteImage} 
+                    onClick={() => onDeleteItem(product.id)}
+                    />
+                </div>
+            </div>
+
+
+        </div>
+
+    );
     }
 
-}
-
 const style = {
-    image : {
+        image : {
          height : 110,
          width : 110,
          borderRadius : 10,
